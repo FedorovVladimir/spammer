08d6b3411e411465ff09a520177c18c60c969a3f
FedorovVladimir <vladimir.fodorow@gmail.com>
2022-02-07 20:10:54 +0700

    clear

--- src/spammer.go
+++ src/spammer.go
@@ -1,17 +1,13 @@
 package src
 
 import (
-	"bufio"
 	"context"
-	"errors"
-	"fmt"
 	"github.com/gotd/td/examples"
 	"github.com/gotd/td/telegram"
 	"github.com/gotd/td/telegram/auth"
 	"github.com/gotd/td/tg"
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
-	"golang.org/x/crypto/ssh/terminal"
 	"log"
 	"math/rand"
 	"os"
@@ -44,12 +40,13 @@ func Run() {
 	log.Println("app run")
 
 	examples.Run(func(ctx context.Context, _ *zap.Logger) error {
-		flow := auth.NewFlow(termAuth{phone: phoneNumber}, auth.SendCodeOptions{})
+		flow := auth.NewFlow(&termAuth{phone: phoneNumber}, auth.SendCodeOptions{})
 
 		client, err := telegram.ClientFromEnvironment(telegram.Options{})
 		if err != nil {
 			return err
 		}
+
 		return client.Run(ctx, func(ctx context.Context) error {
 			if err := client.Auth().IfNecessary(ctx, flow); err != nil {
 				return err
@@ -59,12 +56,13 @@ func Run() {
 
 			// to chats
 			discussion, _ := api.ChannelsGetGroupsForDiscussion(ctx)
-			for _, chatClass := range discussion.GetChats() {
-				switch chat := chatClass.(type) {
-				case *tg.Chat:
-					if contains(chat.Title, names) {
-						chatIDs = append(chatIDs, chat.ID)
-					}
+			for _, chat := range discussion.GetChats() {
+				c, ok := chat.(*tg.Chat)
+				if !ok {
+					continue
+				}
+				if contains(c.Title, names) {
+					chatIDs = append(chatIDs, c.ID)
 				}
 			}
 
@@ -76,36 +74,38 @@ func Run() {
 			date := time.Now().UTC().Unix()
 			for {
 				difference, err := api.UpdatesGetDifference(ctx, &tg.UpdatesGetDifferenceRequest{
-					Flags:         0,
 					Pts:           pts,
 					PtsTotalLimit: 10,
 					Date:          int(date),
-					Qts:           0,
 				})
 				if err != nil {
 					return err
 				}
 				date = time.Now().UTC().Unix()
-				switch d := difference.(type) {
-				case *tg.UpdatesDifference:
-					for _, channel := range d.Chats {
-						switch c := channel.(type) {
-						case *tg.Channel:
-							if strings.ToLower(c.Title) != "рассылка" {
-								continue
-							}
-							for _, update := range d.OtherUpdates {
-								switch u := update.(type) {
-								case *tg.UpdateNewChannelMessage:
-									if m, ok := u.Message.(*tg.Message); ok {
-										sendMessage(ctx, api, m, chatIDs)
-									}
-								}
-							}
-						}
-					}
-					pts = d.State.Pts
+
+				d, ok := difference.(*tg.UpdatesDifference)
+				if !ok {
+					continue
 				}
+				for _, chat := range d.Chats {
+					c, ok := chat.(*tg.Channel)
+					if !ok || strings.ToLower(c.Title) != "рассылка" {
+						continue
+					}
+					for _, update := range d.OtherUpdates {
+						u, ok := update.(*tg.UpdateNewChannelMessage)
+						if !ok {
+							continue
+						}
+						m, ok := u.Message.(*tg.Message)
+						if !ok {
+							continue
+						}
+						sendMessages(ctx, api, m, chatIDs)
+					}
+				}
+				pts = d.State.Pts
+
 				time.Sleep(time.Second)
 			}
 		})
@@ -114,7 +114,7 @@ func Run() {
 
 var mID = 0
 
-func sendMessage(ctx context.Context, api *tg.Client, message *tg.Message, chatIDs []int64) {
+func sendMessages(ctx context.Context, api *tg.Client, message *tg.Message, chatIDs []int64) {
 	if message.ID != mID {
 		for _, chatID := range chatIDs {
 			_, _ = api.MessagesSendMessage(ctx, &tg.MessagesSendMessageRequest{
@@ -134,44 +134,4 @@ func contains(name string, names []string) bool {
 		}
 	}
 	return false
-}
-
-// noSignUp can be embedded to prevent signing up.
-type noSignUp struct{}
-
-func (c noSignUp) SignUp(_ context.Context) (auth.UserInfo, error) {
-	return auth.UserInfo{}, errors.New("not implemented")
-}
-
-func (c noSignUp) AcceptTermsOfService(_ context.Context, tos tg.HelpTermsOfService) error {
-	return &auth.SignUpRequired{TermsOfService: tos}
-}
-
-// termAuth implements authentication via terminal.
-type termAuth struct {
-	noSignUp
-
-	phone string
-}
-
-func (a termAuth) Phone(_ context.Context) (string, error) {
-	return a.phone, nil
-}
-
-func (a termAuth) Password(_ context.Context) (string, error) {
-	fmt.Print("Enter 2FA password: ")
-	bytePwd, err := terminal.ReadPassword(0)
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimSpace(string(bytePwd)), nil
-}
-
-func (a termAuth) Code(_ context.Context, _ *tg.AuthSentCode) (string, error) {
-	fmt.Print("Enter code: ")
-	code, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimSpace(code), nil
 }
